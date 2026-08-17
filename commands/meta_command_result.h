71568a1f400c42a16bfa524150620935a934c2e9
Marcos9868 <marcos_m_ferreira@outlook.com>
2026-08-17 19:39:23 -0300

    Feat: commands
    Add and segregate all command files here

---
+++ commands/meta_command_result.h
@@ -0,0 +1,7 @@
+#ifndef META_COMMAND_RESULT_H
+#define META_COMMAND_RESULT_H
+#include "enums/meta_command_result_enum.h"
+#include "structs/input_buffer.h"
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
+#endif
